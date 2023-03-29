9abd67edb1eee37d3c8098b143b2fe0971efeadd
Jozoko <jozo1@yandex.ru>
2023-03-29 07:05:40 +0300

    1.1.102 fixes

--- auth.php
+++ auth.php
@@ -1,6 +1,5 @@
 <?php
 header('Content-Type: application/json');
-require 'config.php';
 require './controllers/authController.php';
 
 $username = $_POST['username'];
--- models/authModel.php
+++ models/authModel.php
@@ -3,6 +3,61 @@ require 'config.php';
 
 class AuthModel
 {
+    public function authenticate($username, $password): array
+    {
+        global $ldap_host, $ldap_port, $ldap_domain, $ldap_group;
+        $response = [
+            'success' => false,
+            'error' => 'Ошибка: Неизвестная ошибка',
+        ];
+
+        if (empty($username) || empty($password)) {
+            $response['error'] = 'Ошибка: Пожалуйста, введите логин и пароль';
+            return $response;
+        }
+
+        $ldap_conn = $this->connectToLDAP($ldap_host, $ldap_port);
+        if (!$ldap_conn) {
+            $response['error'] = 'Ошибка: Невозможно подключиться к серверу LDAP';
+            return $response;
+        }
+
+        $ldap_bind = @ldap_bind($ldap_conn, $username . "@" . $ldap_domain, $password);
+
+        if (!$ldap_bind) {
+            $response['error'] = 'Ошибка: Неверный логин или пароль';
+            ldap_unbind($ldap_conn);
+            return $response;
+        }
+
+        $userdn = $this->getUserDN($ldap_conn, $username);
+        $groupdn = $this->getGroupDN($ldap_conn, $ldap_group);
+
+        if ($userdn && $groupdn) {
+            $auth = $this->checkGroupEx($ldap_conn, $userdn, $groupdn);
+
+            if ($auth) {
+                $response['success'] = true;
+                $response['error'] = '';
+            } else {
+                $response['error'] = 'Ошибка: Пользователь или группа не найдены в AD';
+            }
+        } else {
+            $response['error'] = 'Ошибка: Пользователь или группа не найдены в AD';
+        }
+
+        ldap_unbind($ldap_conn);
+        return $response;
+    }
+
+    private function connectToLDAP($host, $port)
+    {
+        $ldap_conn = ldap_connect($host, $port);
+        ldap_set_option($ldap_conn, LDAP_OPT_PROTOCOL_VERSION, 3);
+        ldap_set_option($ldap_conn, LDAP_OPT_REFERRALS, 0);
+        return $ldap_conn;
+    }
+
     public function getUserDN($ldap_conn, $username)
     {
         global $ldap_base_dn;
